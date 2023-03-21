beeded3a501e7132dc54e9ec783344902d35a9c8
wu000168 <wu000168@umn.edu>
2023-03-20 23:57:24 -0400

    Progress Proof

--- Exp_lemmas.v
+++ Exp_lemmas.v
@@ -56,45 +56,19 @@ Local Open Scope exp_scope.
 (** Lemmas, manually added *)
 (*************************************************************************)
 
-(* Note: this doesn't compile even when surrounding exp_lit with <{ }> *)
-(* Lemma canonical_forms_int : forall (t : tm), 
-  typing empty t Int ->
-  is_value t ->
-  exists (i : integer), t = <{ exp_lit i }>.
-Proof.
-  intros t HT HVal.
-  destruct HVal. *)
-
-Theorem typing_inversion_gen: forall G t T1,
-  typing G t (ty_poly_poly_gen T1) ->
-    typing G t T1.
-Proof with eauto.
-  intros. inversion H; subst.
-  - admit.
-Admitted.
-
-Theorem unique_typing_rho: forall G t T1 T2,
-  typing G t (ty_poly_rho T1)
-    -> typing G t (ty_poly_rho T2)
-    -> T1 = T2.
-Proof with eauto.
-  intros G t T1 T2 H1 H2.
-  destruct T1. destruct T2. f_equal.
-  generalize dependent tau0.
-  induction t; intros.
-  - inversion H1; inversion H2; subst...
-    +
-Admitted.
-
-Theorem canonical_forms_fun: forall G t T1 T2,
-  typing G t (ty_poly_rho (ty_rho_tau (ty_mono_func T1 T2)))
+Theorem canonical_forms_fun: forall t T1 T2,
+  typing empty t (ty_poly_rho (ty_rho_tau (ty_mono_func T1 T2)))
     -> is_value_of_tm t 
     -> exists u, t = exp_abs u.
 Proof with eauto.
-  intros. induction t...
-  - (* exp_lit i = exp_abs u *)
-    induction H...
-  
+  intros. assert (Htyp := H).
+  remember (ty_poly_rho (ty_rho_tau (ty_mono_func T1 T2))) as T.
+  remember empty as G.
+  induction H; subst; try discriminate; try destruct H0.
+  - eexists...
+  - apply IHtyping...
+    destruct rho.
+    admit.
 Admitted.
 
 Lemma empty_ctx_typing_lc: forall e T,
@@ -109,14 +83,6 @@ Qed.
 
 #[export] Hint Resolve empty_ctx_typing_lc : core.
 
-Lemma empty_ctx_typing_closed: forall e T,
-  typing empty e T -> fv_tm e [=] {}.
-Proof with eauto.
-  intros e T H.
-  assert (Hlc: lc_tm e)...
-  induction H; subst; simpl in *; try fsetdec.
-Admitted.
-
 Theorem progress : forall e T,
   typing empty e T ->
     is_value_of_tm e \/ exists e', step e e'.
@@ -127,10 +93,10 @@ Proof with eauto.
   try (left; constructor)...
   - right; destruct IHtyping1; destruct IHtyping2...
     + assert (Hlam: exists u, t = exp_abs u).
-      { apply (canonical_forms_fun empty t _ _ H0_ H0). }
+      { apply (canonical_forms_fun t _ _ H0_ H0). }
       destruct Hlam; subst...
     + assert (Hlam: exists u, t = exp_abs u).
-      { apply (canonical_forms_fun empty t _ _ H0_ H0). }
+      { apply (canonical_forms_fun t _ _ H0_ H0). }
       destruct Hlam as [t1 Hlam]; subst...
       destruct H1 as [u' H1]. eexists...
     + destruct H0 as [t' H0]. eexists...
@@ -150,4 +116,7 @@ Theorem preservation : forall (E : ctx) e e' T,
     -> step e e'
     -> typing E e' T.
 Proof.
-  intros E e e' T Htyp Hstep. 
+  intros E e e' T Htyp Hstep.
+
+
+Admitted.

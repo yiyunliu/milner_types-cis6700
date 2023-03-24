9d481ea95598f19fda9c9e610ce490e6cef19b91
Hanxi Chen <garychen2000@outlook.com>
2023-03-24 16:04:24 -0400

    Checkpoint: progres on canonical form func

--- Exp_lemmas.v
+++ Exp_lemmas.v
@@ -65,12 +65,57 @@ Proof.
   intros t HT HVal.
   destruct HVal. *)
 
+Lemma exp_abs_not_base : forall (Gamma: ctx) (t : tm),
+    not (typing Gamma (exp_abs t) Int).
+Proof.
+  intros Gamma t H. remember (exp_abs t) as t_fun. induction H; try (inversion Heqt_fun).
+  - subst. simpl in *. pick fresh x0 for L. apply H0 in Fr; auto. admit.
+  - pick fresh x0 for L. apply H0 in Fr; auto.
+  - auto.
+Admitted.
+
+Lemma exp_typed_abs_not_base : forall Gamma (t : tm) T,
+    not (typing Gamma (exp_typed_abs T t) Int).
+Proof.
+  intros Gamma t T H. remember (exp_typed_abs T t) as t_fun. induction H; try (inversion Heqt_fun).
+  - pick fresh x0 for L. specialize H0 with x0. apply H0; auto.
+  - auto.
+Qed.
+  
+
+Lemma canonical_forms_int : forall (t : tm),
+    typing empty t Int ->
+    is_value_of_tm t ->
+    exists (i : integer), t = exp_lit i.
+Proof with eauto.
+  intros. induction t; try (destruct H0).
+  - exists i. reflexivity.
+  - apply exp_abs_not_base in H. destruct H.
+  - inversion H; subst. apply exp_typed_abs_not_base in H. inversion H.
+Qed.
+
+(* Is this actually correct? Shouldn't we open it wrt to some mono type? *)
 Theorem typing_inversion_gen: forall G t T1,
   typing G t (ty_poly_poly_gen T1) ->
     typing G t T1.
 Proof with eauto.
-  intros. inversion H; subst.
-  - admit.
+  intros. remember (ty_poly_poly_gen T1) as T2. induction H.
+  - inversion HeqT2.
+  - subst. induction T1.
+    +  admit. (* Probably need to use the inst rule here... but how *)
+    + 
+
+
+
+  (* ; try (inversion HeqT2; subst). *)
+  (* -  *)
+
+
+  (* inversion H; subst. *)
+  (* -  *)
+
+
+    admit.
 Admitted.
 
 Theorem unique_typing_rho: forall G t T1 T2,
@@ -86,14 +131,43 @@ Proof with eauto.
     +
 Admitted.
 
+Lemma exp_lit_not_func : forall Gamma i T1 T2,
+    not (typing Gamma (exp_lit i) (ty_poly_rho (ty_rho_tau (ty_mono_func T1 T2)))).
+Proof.
+  intros Gamma i T1 T2 H. remember (exp_lit i) as e_lit. inversion H; subst. Admitted.
+
+
+(*   remember (exp_typed_abs T t) as t_fun. induction H; try (inversion Heqt_fun). *)
+(*   - pick fresh x0 for L. specialize H0 with x0. apply H0; auto. *)
+(*   - auto. *)
+(* Qed. *)
+
+(* I think you can also have typed_abs in there?*)
+Theorem canonical_forms_fun_mod: forall G t T1 T2,
+  typing G t (ty_poly_rho (ty_rho_tau (ty_mono_func T1 T2)))
+    -> is_value_of_tm t 
+    -> (exists u, t = exp_abs u) \/ (exists u sig, t = exp_typed_abs sig u).
+Proof with eauto.
+  intros. induction t; try (destruct H0).
+  - inversion H; subst. admit.
+  - left. exists t. reflexivity.
+  - right. exists t. exists sig. auto.
+Admitted.
+
 Theorem canonical_forms_fun: forall G t T1 T2,
   typing G t (ty_poly_rho (ty_rho_tau (ty_mono_func T1 T2)))
     -> is_value_of_tm t 
-    -> exists u, t = exp_abs u.
+    -> (exists u, t = exp_abs u).
 Proof with eauto.
-  intros. induction t...
-  - (* exp_lit i = exp_abs u *)
-    induction H...
+  intros. induction t; try (destruct H0).
+  - inversion H; subst. admit.
+  - left. exists t. reflexivity.
+  - right. exists t. exists sig.auto
+
+  
+  (* intros. induction t... *)
+  (* - (* exp_lit i = exp_abs u *) *)
+  (*   induction H... *)
   
 Admitted.
 
@@ -150,4 +224,5 @@ Theorem preservation : forall (E : ctx) e e' T,
     -> step e e'
     -> typing E e' T.
 Proof.
-  intros E e e' T Htyp Hstep. 
+  intros E e e' T Htyp Hstep.
+  Admitted.
